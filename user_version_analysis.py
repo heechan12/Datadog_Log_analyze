64cca711814a7bedb0625fe9df0133afdcf87224
heechan_ <heechan12@naver.com>
2025-02-04 11:43:35 +0900

    User 분석 페이지 불필요 차트 제거

--- user_version_analysis.py
+++ user_version_analysis.py
@@ -27,13 +27,13 @@ def user_version_analysis():
             user_counts = df['User Id'].value_counts().reset_index()
             total_unique_users = df['User Id'].nunique()
             user_counts.columns = ['User Id', '개수']
-            st.subheader(f"User Id 별 개수 (중복 제거 User Id 수: {total_unique_users})")
+            st.subheader(f"User Id 별  RTP Timeout 개수")
             top_10_users = user_counts.head(10)
             st.table(top_10_users)
 
             with st.expander("전체 User Id 보기"):
                 st.table(user_counts)
-            st.bar_chart(user_counts.set_index('User Id')['개수'])
+            # st.bar_chart(user_counts.set_index('User Id')['개수'])
         else:
             st.warning("User Id 열이 없습니다.")
 
@@ -43,7 +43,7 @@ def user_version_analysis():
             version_counts.columns = ['버전', '개수']
             st.subheader("버전 별 개수")
             st.table(version_counts)
-            st.bar_chart(version_counts.set_index('버전')['개수'])
+            # st.bar_chart(version_counts.set_index('버전')['개수'])
         else:
             st.warning("Version 열이 없습니다.")
 
